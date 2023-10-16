75de1418567f377d163a4c94d49b424c865eaa55
Ha1972000 <haluu1862k@gmail.com>
2023-10-16 18:01:34 +0700

    editModel

--- lib/model/Contact.dart
+++ lib/model/Contact.dart
@@ -2,6 +2,9 @@ class Contact {
   String name;
   String phoneNum;
   String? group;
+  String? date;
+  String? birthDay;
 
-  Contact({required this.name, required this.phoneNum,this.group,});
+
+  Contact({required this.name, required this.phoneNum,this.group,this.date,this.birthDay});
 }
